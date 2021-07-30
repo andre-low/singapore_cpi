1f4ef15e44bfc26075e8e93bb911ec80af2dcf4d
Andre Low <andre-low@users.noreply.github.com>
2021-07-30 18:30:02 +0800

    Written function to fix dates

--- singapore_cpi.py
+++ singapore_cpi.py
@@ -1,7 +1,17 @@
 import json
 import requests
+import calendar
+from datetime import datetime
 
-response = requests.get("http://tablebuilder.singstat.gov.sg/api/table/tabledata/M212881?resourceId=M212881&seriesno=3&sortBy=seriesno%20desc")
-data = json.loads(response.text)
+def fixSingstatDates() :
+    response = requests.get("http://tablebuilder.singstat.gov.sg/api/table/tabledata/M212881?resourceId=M212881&seriesno=3&sortBy=seriesno%20desc")
+    data = json.loads(response.text)
 
-print(data)
+    for i in data["Data"]["row"][0]["columns"] :
+        date = datetime.strptime(i["key"], "%Y %b")
+        dateMonthEnd = datetime.strptime(str(date.year) + '/' + str(date.month) + '/' + str(calendar.monthrange(date.year, date.month)[1]), "%Y/%m/%d")
+        i["key"] = str(dateMonthEnd)
+
+    return json.dumps(data)
+
+print(fixSingstatDates())
