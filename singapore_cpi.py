f99f64f98fa1991004666548099b83c4e015daa2
Andre Low <andre-low@users.noreply.github.com>
2021-08-02 00:30:40 +0800

    Script changes

--- singapore_cpi.py
+++ singapore_cpi.py
@@ -1,17 +1,18 @@
 import json
-import requests
+import urllib3
 import calendar
 from datetime import datetime
 
-def fixSingstatDates() :
-    response = requests.get("http://tablebuilder.singstat.gov.sg/api/table/tabledata/M212881?resourceId=M212881&seriesno=3&sortBy=seriesno%20desc")
-    data = json.loads(response.text)
+def fixSingstatCpiDates() :
+    http = urllib3.PoolManager()
+    response = http.request('GET', 'http://tablebuilder.singstat.gov.sg/api/table/tabledata/M212881?resourceId=M212881&seriesno=3&sortBy=seriesno%20desc')
+    data = json.loads(response.data)
 
-    for i in data["Data"]["row"][0]["columns"] :
-        date = datetime.strptime(i["key"], "%Y %b")
-        dateMonthEnd = datetime.strptime(str(date.year) + '/' + str(date.month) + '/' + str(calendar.monthrange(date.year, date.month)[1]), "%Y/%m/%d")
-        i["key"] = str(dateMonthEnd)
+    for i in data['Data']['row'][0]['columns'] :
+        date = datetime.strptime(i['key'], '%Y %b')
+        dateMonthEnd = datetime.strptime(str(date.year) + '/' + str(date.month) + '/' + str(calendar.monthrange(date.year, date.month)[1]), '%Y/%m/%d')
+        i['key'] = str(dateMonthEnd)
 
     return json.dumps(data)
 
-print(fixSingstatDates())
+print(fixSingstatCpiDates())
